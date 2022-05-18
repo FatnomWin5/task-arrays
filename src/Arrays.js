4ef1ae3a98e26c84be03881931de1c5f9113f79f
FantomWin5 <danya.paklin18@gmail.com>
2022-05-18 19:01:32 +0300

    задание по task-arrays

--- src/Arrays.js
+++ src/Arrays.js
@@ -3,18 +3,34 @@
   Преобразуте строку в массив чисел, отсортируйте их по возрастанию и верните строку с упорядоченными числами,
   разделенными запятыми.
 */
-export function arraySorting(data) {}
+export function arraySorting(data) {
+    let arr = data.split(',').map(Number);
+    arr.sort((a, b) => a - b);
+    let line = arr.join();
+    return line;
+}
 
 /*
   В функцию arrayFiltering() приходит массив целых чисел.
   Преобразуйте массив таким образом, чтобы все элементы массива, в которых содержатся числа более 100,
   были бы удалены из массива, и верните его в качестве результата функции.
 */
-export function arrayFiltering(data) {}
+export function arrayFiltering(data) {
+    let arr = data.filter((k) => k < 100);
+    return arr;
+}
 
 /*
   В функцию arrayPushing() приходят два массива целых чисел размерностью 5 элементов.
   Получите третий массив размерностью 10 элементов, в котором последовательно чередуются
   значения из первых двух массивов, и верните его в качестве результата функции.
 */
-export function arrayPushing(array1, array2) {}
+export function arrayPushing(array1, array2) {
+    let newarr = [];
+    while (array1.length || array2.length) {
+        newarr.push(array1.shift());
+        newarr.push(array2.shift());
+    }
+
+    return newarr;
+}
